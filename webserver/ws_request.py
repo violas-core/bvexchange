9183bac69f25748bc428d20f9fa74396c1877219
yinzhiqing <yinzhiqing@163.com>
2020-02-22 06:49:46 -0800

    modified webserver: added target chain's get address list and get transaction's detail info

--- webserver/ws_request.py
+++ webserver/ws_request.py
@@ -36,16 +36,92 @@ logger = log.logger.getLogger(mod_name)
 
 @app.route('/')
 def test():
-    return "hello world."
+    return "violas record webserver."
 
-@app.route('/tranrecord/<string:sender>/<int:cursor>/<int:limit>/', methods=['GET'])
-def get_transaction_record(sender, cursor = 0, limit = 99999999):
+
+@app.route('/trandetail/<string:dtype>/<string:version>/', methods=['GET'])
+def get_record_detail(dtype, version):
+    try:
+        logger.debug(f"get record detail(dtype = {dtype}, version={version})")
+        rclient = requestclient("l2vrecord", get_proofdb(dtype))
+
+        ret = rclient.get_tran(version)
+        if ret.state != error.SUCCEED:
+            raise f"get transaction record detail failed dtype = {dtype}, version={version}"
+        datas = {"dtype": dtype,\
+                "version": version, \
+                "datas": ret.datas}
+        ret = result(error.SUCCEED, "", datas)
+    except Exception as e:
+        ret = parse_except(e)
+    return ret.__repr__()
+
+@app.route('/tranaddress/<string:chain>/<int:cursor>/<int:limit>/', methods=['GET'])
+def get_addresses(chain, cursor = 0, limit = 99999999):
+    try:
+        logger.debug(f"get record address(chain = {chain}, cursor={cursor}, limit={limit})")
+
+        check_chain(chain)
+
+        rclient = requestclient("l2vrecord", get_proofdb("record"))
+        ret = rclient.list_record_address_for_chain(chain, cursor = cursor, limit=limit)
+        if ret.state != error.SUCCEED:
+            raise f"get transaction record failed chain = {chain}"
+        datas = {"cursor": ret.datas[0],\
+                "count": len(ret.datas[1]), \
+                "datas": [data[:0 - len(chain) -1] for data in ret.datas[1]]}
+        ret = result(error.SUCCEED, "", datas)
+    except Exception as e:
+        ret = parse_except(e)
+    return ret.__repr__()
+
+@app.route('/tranrecord/<string:chain>/<string:sender>/<int:cursor>/<int:limit>/', methods=['GET'])
+def get_transaction_record(chain, sender, cursor = 0, limit = 99999999):
+    try:
+        logger.debug(f"get record(chain = {chain} sender={sender}, cursor={cursor}, limit={limit})")
+
+        check_chain(chain)
+
+        rclient = requestclient("l2vrecord", get_proofdb("record"))
+        ret = rclient.get_transaction_record(sender, chain, cursor = cursor, limit=limit)
+        if ret.state != error.SUCCEED:
+            raise f"get transaction record failed.chain = {chain}, sender = {sender}, cursor={cursor}, limit={limit}"
+        datas = {"cursor": ret.datas[0],\
+                "count": len(ret.datas[1]), \
+                "datas": ret.datas[1]}
+        ret = result(error.SUCCEED, "", datas)
+    except Exception as e:
+        ret = parse_except(e)
+    return ret.__repr__()
+
+def check_chain(chain):
+    if chain.upper() not in ("VIOLAS", "LIBRA", "BTC"):
+        raise f"{chain} is invalid. must be [VIOLAS, LIBRA, BTC]"
+
+def get_chain_record(flag, sender, cursor, limit):
+    try:
+        flag = "LIBRA"
+        rclient = requestclient("l2vrecord", get_proofdb("record"))
+        ret = rclient.get_transaction_record(sender, flag, cursor = cursor, limit=limit)
+        if ret.state != error.SUCCEED:
+            raise f"get transaction record failed.chain = {flag}, sender = {sender}"
+
+    except Exception as e:
+        ret = parse_except(e)
+    return ret
+def get_proofdb(dtype):
+    return stmanage.get_db(dtype)
+
+'''
+def get_transaction_record(chain, sender, cursor = 0, limit = 99999999):
     try:
         module = None
-        logger.debug(f"get record(sender={sender}, cursor={cursor}, limit={limit})")
-        ret_violas = get_violas_record(sender, cursor, limit)
-        if ret_violas.state != error.SUCCEED:
-            raise "get transaction record failed.{chain = violas, sender = {sender}}"
+        logger.debug(f"get record(chain = {chain} sender={sender}, cursor={cursor}, limit={limit})")
+        if chain.upper() == "VIOLAS":
+            ret_violas = get_violas_record(sender, cursor, limit)
+            if ret_violas.state != error.SUCCEED:
+                raise "get transaction record failed.{chain = violas, sender = {sender}}"
+        elif chain.upper == "LIBRA":
 
         ret_libra = get_libra_record(sender, cursor, limit)
         if ret_libra.state != error.SUCCEED:
@@ -77,9 +153,6 @@ def get_transaction_record(sender, cursor = 0, limit = 99999999):
         ret = parse_except(e)
     return ret.__repr__()
 
-def get_proofdb(dtype):
-    return stmanage.get_db(dtype)
-
 def get_libra_record(sender, cursor, limit):
     try:
         flag = "LIBRA"
@@ -91,7 +164,6 @@ def get_libra_record(sender, cursor, limit):
     except Exception as e:
         ret = parse_except(e)
     return ret
-
 def get_violas_record(sender, cursor, limit):
     try:
         flag = "VIOLAS"
@@ -114,4 +186,4 @@ def get_btc_record(sender, cursor, limit):
     except Exception as e:
         ret = parse_except(e)
     return ret
-
+'''
