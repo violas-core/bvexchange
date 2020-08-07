0f692e674c34342da3b163fa2ca1b6754677a7b7
yinzhiqing <yinzhiqing@163.com>
2020-08-07 09:07:46 +0000

    modified cancel state

--- exchange/v2l.py
+++ exchange/v2l.py
@@ -121,38 +121,48 @@ class v2l(vlbase):
             self.insert_to_localdb_with_check(version, localdb.state.START, tran_id, receiver)
 
         if self.use_module(state, localdb.state.ESUCCEED):
+            self._logger.debug(f"exec_exchange-0. start swap_get_output_amount({from_token_id} {to_token_id} {amount})...")
             ret = self.violas_client.get_balance(combine_account.address.hex(), map_token_id)
             if ret.state != error.SUCCEED:
+                self._logger.error("exec_exchange-0.result: failed.")
                 self.update_localdb_state_with_check(tran_id, localdb.state.FAILED)
                 return ret
             before_amount = ret.datas
             detail.update({"before_amount": before_amount})
 
             #get gas for swap
+            self._logger.debug(f"exec_exchange-1. start swap_get_output_amount({from_token_id} {to_token_id} {amount})...")
             ret = self.violas_client.swap_get_output_amount(from_token_id, map_token_id, amount)
             if ret.state != error.SUCCEED:
                 self.update_localdb_state_with_check(tran_id, localdb.state.FAILED)
+                return ret
+
             out_amount_chian, gas = ret.datas
+            self._logger.debug(f"exec_exchange-1.result : can swap amount: = {out_amount_chian} gas = {gas}, want = {out_amount}{to_token_id}")
 
             #temp value(test)
             if out_amount <= 0:
                 out_amount = out_amount_chian
-            elif out_amount > out_amount_chian: #don't execute swap, Reduce the cost of the budget
+            elif out_amount > out_amount_chian or out_amount_chian <= 0: #don't execute swap, Reduce the cost of the budget
                 self.update_localdb_state_with_check(tran_id, localdb.state.FAILED)
                 return result(error.FAILED, \
                             f"don't execute swap(out_amount({out_amount}) > cur_outamount({out_amount_chian})), Reduce the cost of the budget")
             detail.update({"gas": gas})
 
             #swap LBRXXX -> VLSYYY
+            self._logger.debug(f"exec_exchange-2. start swap({from_token_id}, {map_token_id}, {amount}...")
             ret = self.violas_client.swap(from_sender, from_token_id, map_token_id, amount, \
                     out_amount, receiver = combine_account.address.hex(), \
                     gas_currency_code = from_token_id)
 
+            print("swap ok")
             if ret.state != error.SUCCEED:
                 self.update_localdb_state_with_check(tran_id, localdb.state.FAILED)
+                self._logger.error("exec_exchange-2.result: failed.")
                 return ret
 
             #get swap after amount(map_token_id)
+            self._logger.debug("exec_exchange-x. get balance..")
             ret = self.violas_client.get_balance(combine_account.address.hex(), map_token_id)
             if ret.state != error.SUCCEED:
                 ret = self.violas_client.get_address_version(combine_account.address.hex())
@@ -174,10 +184,12 @@ class v2l(vlbase):
         if self.use_module(state, localdb.state.FILLSUCCEED) or \
                 self.use_module(state, localdb.state.PSUCCEED):
 
+            self._logger.debug(f"exec_exchange-3. start fill_address_token...")
             ret = self.fill_address_token(map_sender.address.hex(), to_token_id, \
                     detail["diff_balance"])
 
             if ret.state != error.SUCCEED:
+                self._logger.error(f"exec_exchange-3. result: failed. {ret.message}")
                 self.update_localdb_state_with_check(tran_id, localdb.state.FILLFAILED, \
                         json.dumps(detail))
                 return ret
@@ -186,10 +198,12 @@ class v2l(vlbase):
             markdata = self.libra_client.create_data_for_mark(self.libra_chain, self.dtype, \
                     tran_id, version)
 
+            self._logger.debug(f"exec_exchange-4. start send {to_token_id} to {toaddress} amount = { detail['diff_balance']}...")
             ret = self.libra_client.send_coin(map_sender, toaddress, \
                     detail["diff_balance"], to_token_id, data=markdata)
 
             if ret.state != error.SUCCEED:
+                self._logger.error("exec_exchange-4.result: failed.")
                 self.update_localdb_state_with_check(tran_id, localdb.state.PFAILED, \
                         json.dumps(detail))
                 return ret
