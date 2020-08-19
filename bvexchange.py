019e4965be803c443ff1445793c9dd3f19b6e47f
yinzhiqing <yinzhiqing@163.com>
2020-08-19 11:55:28 +0800

    modified violas min valid version

--- bvexchange.py
+++ bvexchange.py
@@ -30,7 +30,7 @@ class works:
     __record_db = "record"
 
     __libra_min_valid_version   = 0
-    __violas_min_valid_version  = 1971_1311
+    __violas_min_valid_version  = 1909_6944
     __btc_min_valid_version     = 0
     def __init__(self):
         logger.debug("works __init__")
