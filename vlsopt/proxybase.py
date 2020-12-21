03a8de1988f22ff0d7d66af5c7dcf37019eb068f
yinzhiqing <yinzhiqing@163.com>
2020-12-21 17:07:58 +0800

    update violas wallet DD account address

--- vlsopt/proxybase.py
+++ vlsopt/proxybase.py
@@ -39,7 +39,6 @@ class proxybase():
         data = data.replace("\n", "")
         data = data.replace("\r", "")
         mnemonic_num = data.split(cls.DELIMITER)
-        print(mnemonic_num)
         if len(mnemonic_num) < 2 or not mnemonic_num[1].isdecimal():
             raise Exception(f"Format error: Wallet must <MNEMONIC_NUM>{cls.DELIMITER}<NUM>[ADDRESS{cls.ASSIGNMENT}DD_ADDRESS;ADDRESS{cls.ASSIGNMENT}DD_ADDRESS]")
         
@@ -54,6 +53,7 @@ class proxybase():
                 
                 if len(address_dd[0]) not in cls.ADDRESS_LEN or \
                         len(address_dd[1]) not in cls.ADDRESS_LEN:
+                        print(address_dd)
                         raise Exception(f"address is invalid. {mnemonic_num[i]}")
                 wallet.replace_address(address_dd[0], address_dd[1])
                 wallet.append_address_map(address_dd[0], address_dd[1])
