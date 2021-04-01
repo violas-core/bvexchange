5adce185024ed0cb9490f443c260466ccfa35528
yinzhiqing <yinzhiqing@163.com>
2021-04-01 07:32:30 -0700

    client can shutdown

--- client/client.py
+++ client/client.py
@@ -17,13 +17,14 @@ def parse_msg(cmd, conn = None, listener = None, **kwargs):
     print("received msg: {}".format(cmd))
 
 
-cli = client("127.0.0.1", 8055)
-cli.start(parse_msg)
+cli = client("127.0.0.1", 8055, call = parse_msg)
+#cli.start(parse_msg)
 time.sleep(1)
-cli.send("smods")
+#cli.send("smods")
 time.sleep(1)
 #cli.send("disconnect")
 cli.send("shutdown")
+time.sleep(1)
 cli.close()
 
 
--- command/parse_cmd.py
+++ command/parse_cmd.py
@@ -4,13 +4,17 @@ import sys
 import json
 import os
 import time
+import types
 sys.path.append(os.getcwd())
 sys.path.append("..")
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "../"))
 
 
 def reponse(msg, conn = None, listener = None, **kwargs):
-    return conn.send(f"{msg}")
+    return conn.send(f"{msg}") if not conn.closed else None
+
+def is_func_or_method(func):
+    return isinstance(func, types.MethodType) or isinstance(func, types.FunctionType)
 
 def show_msg(msg, logger = None):
     if logger:
@@ -18,18 +22,21 @@ def show_msg(msg, logger = None):
     else:
         print(msg)
 
+def call_shutdown(call):
+    call()
+    return False
+
+def get_datas_from_cmd(arg):
+    return arg() if is_func_or_method(arg) else arg
+
 def parse_cmd(cmd, conn = None, listener = None, **kwargs):
     show_msg("received msg: {}".format(cmd), kwargs.get("logger"))
-    support_mods = kwargs.get("support_mods", [])
-    if cmd == "smods":
-        reponse(support_mods, conn)
-    elif cmd == "shutdown":
-        kwargs.get(cmd)()
-        reponse("shutdown...", conn)
-        return False
-
-    elif cmd == "msg 2":
-        reponse("22222222", conn)
+    if cmd in kwargs.keys():
+        if cmd == "shutdown":
+            reponse("shutdown...", conn)
+            ret = get_datas_from_cmd(kwargs.get(cmd))
+        else:
+            reponse(get_datas_from_cmd(kwargs.get(cmd)), conn)
     else:
         return False
     return True
